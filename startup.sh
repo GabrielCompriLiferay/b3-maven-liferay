47d9feb5ae945baeb62d1bb40a7e5de93b621352
Gabriel Compri <gabriel.compri@liferay.com>
2023-12-27 17:17:58 -0300

    Update Configs

--- startup.sh
+++ startup.sh
@@ -2,9 +2,3 @@
 
 mkdir -p ./volumes/database/data
 mkdir -p ./volumes/search/data
-mkdir -p ./volumes/liferay/data
-mkdir -p ./volumes/liferay/mnt
-
-mkdir -p ./bundles/deploy
-mkdir -p ./bundles/osgi/modules
-mkdir -p ./bundles/osgi/war
